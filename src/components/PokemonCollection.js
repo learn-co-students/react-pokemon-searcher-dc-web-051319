7a1537170d2e3805271f5ff1200bd1f33301878a
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-07-23 23:02:43 -0400

    Completed lab

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -7,6 +7,9 @@ class PokemonCollection extends React.Component {
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {this.props.allPokemon.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemonObj={pokemon}/>
+        ))}
       </Card.Group>
     )
   }
